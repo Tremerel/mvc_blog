8643183bb8ccc6b2fe99ae42686d370d7a8843fa
Tremerel <84023901+Tremerel@users.noreply.github.com>
2021-12-06 18:42:31 +0100

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+    // Database params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'blog');
+
+    // APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URLROOT (dynamic links)
+    define('URLROOT', 'http://localhost/mvc_pdo/'); 
+
+    // Sitename
+    define('SITENAME', 'SF-modeling');
+
+    // Online Version
+    // Database params
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'id17451623_moi');
+    // define('DB_PASS', 'RWz#Ei!@Q6ur&y1I');
+    // define('DB_NAME', 'id17451623_blog');
+
+    // // APPROOT
+    // define('APPROOT', dirname(dirname(__FILE__)));
+
+    // // URLROOT (dynamic links)
+    // define('URLROOT', 'https://correlative-inquiry.000webhostapp.com/'); 
+
+    // // Sitename
+    // define('SITENAME', 'TremDev');
+
+?>
---
+++ app/views/contact_us/index.php
@@ -0,0 +1,5 @@
+<?php require_once APPROOT . '/views/includes/head.php'; ?>
+
+<div class="navbar">
+    <?php require_once APPROOT . '/views/includes/navigation.php'; ?>
+</div>
